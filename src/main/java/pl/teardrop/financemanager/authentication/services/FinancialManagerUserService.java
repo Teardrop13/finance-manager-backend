b09c3bc5c0e27abef5572c65221e9c72969d1e7e
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-04-15 21:48:47 +0200

    Refactor adding categories after user registration

--- src/main/java/pl/teardrop/financemanager/authentication/services/FinancialManagerUserService.java
+++ src/main/java/pl/teardrop/financemanager/authentication/services/FinancialManagerUserService.java
@@ -4,19 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import pl.teardrop.authentication.user.DefaultUserService;
 import pl.teardrop.authentication.user.User;
+import pl.teardrop.authentication.user.UserService;
 import pl.teardrop.financemanager.service.CategoryService;
 
 @Primary
-@Service
+@Service("userService")
 @Slf4j
 @RequiredArgsConstructor
-public class UserService extends pl.teardrop.authentication.user.UserService {
+public class FinancialManagerUserService extends DefaultUserService implements UserService {
 
 	private final CategoryService categoryService;
 
 	@Override
-	protected void runAfterUserCreation(User user) {
-		categoryService.addDefaultCategorires(user);
+	public User create(String username, String password, String email) {
+		User userAdded = super.create(username, password, email);
+		categoryService.addDefaultCategorires(userAdded);
+		return userAdded;
 	}
 }

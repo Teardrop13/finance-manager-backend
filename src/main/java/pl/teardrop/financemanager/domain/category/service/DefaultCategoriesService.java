141f47864162405c3367c2343c76d4f0449db716
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-09-23 10:34:27 +0200

    fix adding default categories after registration

--- src/main/java/pl/teardrop/financemanager/domain/category/service/DefaultCategoriesService.java
+++ src/main/java/pl/teardrop/financemanager/domain/category/service/DefaultCategoriesService.java
@@ -2,23 +2,22 @@ package pl.teardrop.financemanager.domain.category.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import pl.teardrop.authentication.user.UserId;
 import pl.teardrop.financemanager.common.JsonUtil;
+import pl.teardrop.financemanager.domain.category.repository.CategoryRepository;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class DefaultCategoriesService {
 
-	private final CategoryService categoryService;
+	private final CategoryRepository categoryRepository;
 
-	@PreAuthorize("#userId.getId() == authentication.principal.getId()")
 	public void addDefaults(UserId userId) {
 		new JsonUtil().loadDefaultCategories().forEach(category -> {
 			category.setUserId(userId);
-			categoryService.save(category);
+			categoryRepository.save(category);
 		});
 		log.info("Added default categories for userId={}", userId.getId());
 	}
---
+++ src/test/java/pl/teardrop/financemanager/domain/category/service/DefaultCategoriesServiceTest.java
@@ -0,0 +1,51 @@
+package pl.teardrop.financemanager.domain.category.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.teardrop.authentication.user.UserId;
+import pl.teardrop.financemanager.domain.category.model.Category;
+import pl.teardrop.financemanager.domain.category.repository.CategoryRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class DefaultCategoriesServiceTest {
+
+	@Mock
+	private CategoryRepository categoryRepository;
+
+	private DefaultCategoriesService defaultCategoriesService;
+
+	@BeforeEach
+	void setUp() {
+		defaultCategoriesService = new DefaultCategoriesService(categoryRepository);
+	}
+
+	@Test
+	void addDefault() {
+		UserId userId = new UserId(1L);
+		ArgumentCaptor<Category> commandCaptor = ArgumentCaptor.forClass(Category.class);
+
+		defaultCategoriesService.addDefaults(userId);
+
+		verify(categoryRepository, atLeastOnce()).save(commandCaptor.capture());
+
+		List<Category> savedCategories = commandCaptor.getAllValues();
+		assertFalse(savedCategories.isEmpty());
+
+		Category category = savedCategories.get(0);
+		assertNotNull(category.getPriority());
+		assertNotNull(category.getName());
+		assertNotNull(category.getType());
+	}
+
+}
